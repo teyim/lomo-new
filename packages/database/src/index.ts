9ddc50397ba8559909fd4d445ae25050e777f107
teyim <teyimasobo@gmail.com>
2025-07-25 18:40:22 +0100

    fix: build error

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -8,4 +8,6 @@ export const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
-export * from "@prisma/client";
+export * from '@prisma/client';
+
+export { type Asset, type Background } from '@prisma/client';
